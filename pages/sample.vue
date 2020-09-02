b027558bf1cea5b01dc58c282fb8acc04408cf12
nishina555 <n.toshi.1.014@gmail.com>
2020-09-02 09:54:50 +0900

    sampleページの作成

---
+++ pages/sample.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <p>{{ count }}</p>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  width: 50%;
+}
+</style>
